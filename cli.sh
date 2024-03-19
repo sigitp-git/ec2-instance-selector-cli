b5771466ec475edf2bddf883392ec9a7b09eb625
sigitp <sigit.priyanggoro@gmail.com>
2024-03-19 15:25:08 -0500

    Create cli.sh

---
+++ cli.sh
@@ -0,0 +1,1 @@
+ec2-instance-selector --region us-east-1 --vcpus-min 19 --memory-min 80 --hypervisor nitro --network-interfaces-min 10 --output table-wide --sort-by OndemandPricePerHour --max-results 400
